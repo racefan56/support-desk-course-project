ba4fca7e9c8b82d74fc63099f9c60f61b24b9aa1
Nicholas Smith <hamtorokaton@hotmail.com>
2022-03-22 10:27:21 -0500

    Finsihed basic backend structure and initialized react front end

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,26 @@
-console.log('server...');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const colors = require('colors');
+const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
+const PORT = process.env.PORT || 5000;
+
+//CONNECT TO DB
+connectDB();
+
+const app = express();
+
+//MIDDLEWARE
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to the Support desk API' });
+});
+
+// ROUTES
+app.use('/api/users', require('./routes/userRoutes'));
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
